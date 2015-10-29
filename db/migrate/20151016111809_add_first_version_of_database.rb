c68b01ef729c5d6670565d938fad5f24b1f3053d
Kyrre Havik Eriksen <kyrre.eriksen@ciber.com>
2015-10-29 14:39:40 +0100

    Functional database with what we possible need

--- db/migrate/20151016111809_add_first_version_of_database.rb
+++ db/migrate/20151016111809_add_first_version_of_database.rb
@@ -1,18 +1,45 @@
 class AddFirstVersionOfDatabase < ActiveRecord::Migration
   def change
     create_table :seasons do | t |
+      t.integer :year, limit: 2, null: false
+      t.string  :title
+
+      t.timestamps null: false
     end
 
     create_table :events do | t |
+      t.belongs_to :season, index: true
+
+      t.date   :date,  null: false
+      t.string :title, null: false
+      t.string :sport, null: false
+      t.string :venue
+
+      t.timestamps null: false
     end
 
     create_table :participants do | t |
+      t.belongs_to :event, index: true
+      t.belongs_to :user, index: true
+
+      t.timestamps null: false
     end
 
     create_table :results do | t |
+      t.belongs_to :participant, index: true
+      t.belongs_to :event, index: true
+
+      t.integer    :rank, null: false
+      t.string     :score
+
+      t.timestamps null: false
     end
 
     create_table :users do | t |
+      t.string :title, null: false
+      t.string :email
+
+      t.timestamps null: false
     end
   end
 end
---
+++ db/schema.rb
@@ -0,0 +1,67 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20151016111809) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.integer  "season_id"
+    t.date     "date",       null: false
+    t.string   "title",      null: false
+    t.string   "sport",      null: false
+    t.string   "venue"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "events", ["season_id"], name: "index_events_on_season_id", using: :btree
+
+  create_table "participants", force: :cascade do |t|
+    t.integer  "event_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "participants", ["event_id"], name: "index_participants_on_event_id", using: :btree
+  add_index "participants", ["user_id"], name: "index_participants_on_user_id", using: :btree
+
+  create_table "results", force: :cascade do |t|
+    t.integer  "participant_id"
+    t.integer  "event_id"
+    t.integer  "rank",           null: false
+    t.string   "score"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
+
+  add_index "results", ["event_id"], name: "index_results_on_event_id", using: :btree
+  add_index "results", ["participant_id"], name: "index_results_on_participant_id", using: :btree
+
+  create_table "seasons", force: :cascade do |t|
+    t.integer  "year",       limit: 2, null: false
+    t.string   "title"
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "title",      null: false
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
