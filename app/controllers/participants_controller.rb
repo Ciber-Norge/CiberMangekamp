4ee0ddb84ba1c63d9a3b15d3b9bf88eaf366c29f
Kyrre Havik Eriksen <kyrre.eriksen@ciber.com>
2015-12-04 16:59:03 +0100

    Can't remember what I did, just pushing it...

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,10 +1,11 @@
 class ParticipantsController < ApplicationController
   def create
-    p params
     @event = Event.find(params[:event_id])
-    @participant = @event.participants.create(user_params)
-    if @participant.save
-      redirect_to season_event_path(@event.season_id, @event.id)
+    unless @event.participants.find(user_params)
+      @participant = @event.participants.create(user_params)
+      if @participant.save
+        redirect_to season_event_path(@event.season_id, @event.id)
+      end
     end
   end
 
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,5 @@
 class Event < ActiveRecord::Base
   belongs_to :season
   has_many :participants
-  has_many :users, through: :participants
   has_many :results
 end
