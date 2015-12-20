090fe4c880a6230e110853f5b3ea6e1c8bf9bf08
Kyrre Havik Eriksen <kyrremann@gmail.com>
2015-12-20 22:23:34 +0100

    Can now add and delete participants

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,12 +1,24 @@
 class ParticipantsController < ApplicationController
   def create
-    @event = Event.find(params[:event_id])
-    unless @event.participants.find(user_params)
-      @participant = @event.participants.create(user_params)
-      if @participant.save
-        redirect_to season_event_path(@event.season_id, @event.id)
+    event = Event.find(params[:event_id])
+    p params
+    unless event.participants.find_by_user_id(params[:user][:user_id])
+      participant = event.participants.create(user_params)
+      if participant.save
+        redirect_to season_event_path(event.season_id, event.id) and return
       end
     end
+
+    redirect_to season_event_path(event.season_id, event.id)
+  end
+
+  def destroy
+    event = Event.find(params[:event_id])
+    participant = Participant.find(params[:id])
+    if participant
+      participant.destroy
+    end
+    redirect_to season_event_path(event.season_id, event.id)
   end
 
   private
