b27aefd550104875c2510d75fbceb8b998e30dbf
Kyrre Havik Eriksen <kyrremann@gmail.com>
2015-12-26 23:36:04 +0100

    Added DRY form and edit/delete of events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,11 +1,13 @@
 class EventsController < ApplicationController
+  before_filter :find_event, only: [:show, :edit, :update, :destroy]
+
   def show
-    @event = Event.find(params[:id])
     @users = User.order('name ASC')
   end
 
   def new
     @event = Event.new
+    @event.season_id = params[:season_id]
   end
 
   def create
@@ -13,12 +15,34 @@ class EventsController < ApplicationController
     @event = @season.events.create(event_params)
     if @event.save
       redirect_to @event
+    else
+      render  'new'
     end
+  end
+
+  def edit
+  end
+
+  def update
+    if @event.update(event_params)
+      redirect_to @event
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @event.destroy
+    redirect_to root_path
   end
 
   private
 
   def event_params
     params.require(:event).permit(:title, :sport, :date, :venue)
+  end
+
+  def find_event
+    @event = Event.find(params[:id])
   end
 end
