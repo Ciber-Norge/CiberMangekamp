01f840c9fef77fd0d7ef436b2e6a232b80298e53
Kyrre Havik Eriksen <kyrremann@gmail.com>
2015-12-26 23:32:40 +0100

    Added DRY form and edit/destroy of seasons

--- app/controllers/seasons_controller.rb
+++ app/controllers/seasons_controller.rb
@@ -1,10 +1,11 @@
 class SeasonsController < ApplicationController
+  before_filter :find_season, only: [:show, :edit, :update, :destroy]
+
   def index
     @seasons = Season.order('year DESC')
   end
 
   def show
-    @season = Season.find(params[:id])
   end
 
   def new
@@ -15,12 +16,34 @@ class SeasonsController < ApplicationController
     @season = Season.new(season_params)
     if @season.save
       redirect_to @season
+    else
+      render 'new'
     end
+  end
+
+  def edit
+  end
+
+  def update
+    if @season.update(season_params)
+      redirect_to @season
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @season.destroy
+    redirect_to root_path
   end
 
   private
 
   def season_params
     params.require(:season).permit(:title, :year)
+  end
+
+  def find_season
+    @season = Season.find(params[:id])
   end
 end
