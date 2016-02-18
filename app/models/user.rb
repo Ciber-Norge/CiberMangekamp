52cf2b4b7ff1ce1fd56dfc7ba2d656596aa2f38c
Kyrre Havik Eriksen <Kyrremann@gmail.com>
2016-02-18 21:04:58 +0100

    Lagt til scope for default order, og men og kvinner

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   has_many :participants
+
+  default_scope -> { order('name ASC') }
+  scope :men, -> { where(sex: 'man') }
+  scope :women, -> { where(sex: 'woman') }
 end
