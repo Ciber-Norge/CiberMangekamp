5bbc597b03fa352ec52c54f392995b06446e062e
Kyrre Havik Eriksen <kyrre.eriksen@ciber.com>
2016-03-09 11:03:15 +0100

    Added scope for men and women, and tables for each

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -2,4 +2,8 @@ class Participant < ActiveRecord::Base
   belongs_to :user
   belongs_to :event
   has_one :result, :dependent => :destroy
+
+  default_scope -> { joins(:user).order('name ASC') }
+  scope :men, -> { joins(:user).where('users.sex = ?', 'man') }
+  scope :women, -> { joins(:user).where('users.sex = ?', 'woman') }
 end
--- app/models/result.rb
+++ app/models/result.rb
@@ -1,7 +1,7 @@
 class Result < ActiveRecord::Base
   belongs_to :participant
 
-  default_scope {
-    order('rank ASC')
-  }
+  default_scope -> { order('rank ASC') }
+  scope :men, -> { joins(:participant).merge(Participant.men) }
+  scope :women, -> { joins(:participant).merge(Participant.women) }
 end
